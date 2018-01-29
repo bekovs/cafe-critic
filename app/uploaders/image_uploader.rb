81bced7ebce889b538e26b5ee9fb9ab3ec06929d
Unknown <bekovs96@gmail.com>
2018-01-29 16:29:33 +0600

    Some navbar and show changes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :medium do
-    process resize_to_fill: [280, 280]
+    process resize_to_fill: [200, 200]
   end
 
   version :actual do
