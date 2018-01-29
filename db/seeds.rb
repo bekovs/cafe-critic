261f608e6804d3c245ec4259c8cf7b7f3c9111f3
Unknown <bekovs96@gmail.com>
2018-01-29 12:43:53 +0600

    Added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 admn_usr = User.create(name: "Admin", email: "admin@admin.com", password: "qweqweqwe", password_confirmation: "qweqweqwe", admin: true )
+usr = User.create(name: "John", email: "john@gmail.com", password: "asdasdasd", password_confirmation: "asdasdasd")
+
+cafes = Shop.create([{ title: "Dexter Burger", category_id: 2 }, { title: "Little Bay Restaurant", category_id: 1 }, { title: "Dan Thai", category_id: 2 }, { title: "Karnavar Restaurant", category_id: 1 }, { title: "Planet Pizza", category_id: 3 }, { title: "Firebellys", category_id: 3 }])
+
+catgrys = Category.create([{ title: "Restaurant" }, { title: "Fast Food" }, { title: "Pizzeria" } ])
+
